093bf04980e0d29ef940443afc4a46c65fb79fcc
netanelpr <pratn@post.bgu.ac.il>
2020-03-12 15:23:24 +0200

    Task 1a
    Complete task 1a

--- lab1/encoder.c
+++ lab1/encoder.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 
+void task_1a(int argc, char **argv){
+
+	int input = -1;
+	input = fgetc(stdin);
+
+	while(input > -1){
+		
+		if(input > 96 & input < 123){
+			input = input - 32;
+		}
+		fputc(input, stdout);
+		input = fgetc(stdin);
+	}
+}
+
 int main(int argc, char **argv){
+	task_1a(argc, argv);
 	return 0;
 }
