82f34f83c7e31173a99f1512d077107a7a94bbcf
netanelpr <pratn@post.bgu.ac.il>
2020-05-28 13:19:17 +0300

    Complete lab7 task1c
    Complete lab7 task1c

--- lab7/task1/hexeditplus.c
+++ lab7/task1/hexeditplus.c
@@ -3,12 +3,16 @@
 #include <string.h>
 #include <fcntl.h>
 
+#define DECIMAL_DISPLAY 0
+#define HEX_DISPLAY 1
+
 typedef struct {
   char debug_mode;
   char file_name[128];
   int unit_size;
   unsigned char mem_buf[10000];
   size_t mem_count;
+  char dispaly_mode;
 } state;
 
 void freeStateStruct(state *st){
@@ -33,6 +37,14 @@ void debug_print(state *st){
 void debug_string_print(char debug_mode, char *str){
     if(debug_mode != 0){
     fprintf(stderr, "DEBUG: %s\n", str);
+    }
+}
+
+void print_dispaly_mode(char display_mode){
+    if(display_mode == DECIMAL_DISPLAY){
+        printf("Decimal\n");
+    } else {
+        printf("Hexadecimal\n");
     }
 }
 
@@ -117,6 +129,91 @@ void load_into_memory(state *st){
     fclose(file);
 }
 
+void toggle_display_mode(state *st){
+    if(st->dispaly_mode == DECIMAL_DISPLAY){
+        st->dispaly_mode = HEX_DISPLAY;
+        printf("Display flag now on, hexadecimal representation\n");
+    } else {
+        st->dispaly_mode = DECIMAL_DISPLAY;
+        printf("Display flag now off, decimal representation\n");  
+    }
+}
+
+char* unit_to_format(int unit) {
+    static char* formats[] = {"%#hhx\n", "%#hx\n", "No such unit", "%#x\n"};
+    return formats[unit-1];
+}  
+
+void print_units(FILE* output, char* buffer, int count, int unit_size, char display_mode) {
+    char* end = buffer + unit_size * count;
+    while (buffer < end) {
+        //print ints
+        int var = *((int*)(buffer));
+        if(display_mode == DECIMAL_DISPLAY){
+            fprintf(output, "%d\n", var);
+        } else {
+            fprintf(output, unit_to_format(unit_size), var);
+        }
+        buffer += unit_size;
+    }
+}
+
+
+void get_len_and_addr(int *len, size_t *addr){
+    char input[128];
+    fgets(input, 128, stdin);
+    sscanf(input, "%d %x", len, addr);
+}
+
+void dispaly_memory_from_state(state *st, int len){    
+    print_dispaly_mode(st->dispaly_mode);
+    printf("==========\n");
+    print_units(stdout, (char *)st->mem_buf, len, st->unit_size, st->dispaly_mode);
+}
+
+void dispaly_memory_from_the_file(state *st, int len, size_t addr){   
+    FILE *file;
+    char *buff = NULL;
+
+    file = fopen(st->file_name, "r");
+    if(file == NULL){
+        perror("Open file");
+        return;
+    }
+
+    fseek(file, addr, SEEK_SET);
+    buff = (char *)malloc(len * st->unit_size);
+    if(buff == NULL){
+        perror("Malloc");
+        return;
+    }
+    fread(buff, len, st->unit_size, file);
+
+    print_dispaly_mode(st->dispaly_mode);
+    printf("==========\n");
+    print_units(stdout, buff, len, st->unit_size, st->dispaly_mode);
+
+    free(buff);
+    fclose(file);
+}
+
+void memory_display(state *st){
+    int len = 0;
+    size_t addr = 0;
+
+    if(st->file_name == NULL){
+        fprintf(stderr, "The file name is not set");
+        return;
+    }
+
+    get_len_and_addr(&len, &addr);
+    if(addr == 0){
+        dispaly_memory_from_state(st, len);
+    } else {
+        dispaly_memory_from_the_file(st, len, addr-1);
+    }
+}
+
 void quit(state *st){
     freeStateStruct(st);
     exit(0);
@@ -134,6 +231,7 @@ void menu(){
     state *st;
     fun_desc function_desc[] = {{"Toggle Debug Mode", toggle_debug_mode}, {"Set File Name", set_file_name},
                                 {"Set Unit Size", set_unit_size}, {"Load Into Memory", load_into_memory},
+                                {"toggle Display Mode", toggle_display_mode}, {"Memory Display", memory_display},
                                 {"Quit", quit}, {NULL, NULL}};
     
     int menu_size = (int)(sizeof(function_desc)/sizeof(fun_desc) - 1);
