cbbb77fd9399e4cd1873882196dbd7fac8edcc1b
netanelpr <pratn@post.bgu.ac.il>
2020-06-04 11:09:52 +0300

    Complete task2 lab8
    Complete task2 lab8

--- lab8/myELF.c
+++ lab8/myELF.c
@@ -127,6 +127,60 @@ void print_section_headers(Elf32_Shdr *header, uint32_t section_t_size, char *sh
 
 }
 
+typedef struct{
+    Elf32_Sym *symble_table;
+    uint32_t size;
+    char *strtab;
+} Symble_table;
+
+
+void fill_symble_table_struct(Symble_table *st, void *s_map_ptr, Elf32_Shdr *header, uint32_t section_t_size, char *shst){
+    uint32_t index = 0;
+
+    for(; index < section_t_size; ++index){
+        if(header->sh_type == SHT_SYMTAB){
+            st->symble_table = s_map_ptr + header->sh_offset;
+            printf("%x %x\n",header->sh_size, sizeof(Elf32_Sym));
+            st->size = header->sh_size / sizeof(Elf32_Sym);
+            break;
+        }
+        ++header;
+    }
+
+    for(index = 0; index < section_t_size; ++index){
+        if(header->sh_type == SHT_STRTAB){
+            if(strcmp(".strtab", (shst + header->sh_name)) == 0){
+                st->strtab = s_map_ptr + header->sh_offset;
+                break;
+            }
+        }
+        ++header;
+    }
+}
+
+void print_symbol_table(Symble_table *st, Elf32_Shdr *header, char *shst){
+    uint32_t index = 0;
+    Elf32_Sym *symble_t = st->symble_table;
+    Elf32_Shdr *s_header;
+
+    for(; index < st->size; ++index){
+        s_header = header + symble_t->st_shndx;
+
+        printf("[%u]\t",index);
+        printf("%x ", symble_t->st_value);
+        printf("%d ", symble_t->st_shndx);
+        if((symble_t->st_shndx == SHN_UNDEF) | (symble_t->st_shndx == SHN_ABS)){
+            printf("N ");
+        } else {
+            printf("%s ", (shst + s_header->sh_name));
+        }
+        printf("%s ", (st->strtab + symble_t->st_name));
+        printf("\n");
+
+        ++symble_t;
+    }   
+}
+
 //+++++++++++ hexeditplus +++++++++++//
 
 void debug_string_print(char debug_mode, char *str){
@@ -220,12 +274,63 @@ void print_section_names(){
         return;
     }
     header = file_map;
-    printf("%x %x\n", header->e_shoff, header->e_shstrndx);
+
     section_header = file_map + header->e_shoff;
     section_shst_enrty = section_header + header->e_shstrndx;
     shst = file_map + section_shst_enrty->sh_offset;
 
     print_section_headers(section_header, header->e_shnum, shst);
+
+    munmap(file_map, sb.st_size);
+    close(fd);
+
+    return;
+} 
+
+void print_symbols(){
+    void *file_map = NULL;
+    Elf32_Ehdr *header;
+    Elf32_Shdr *section_header, *section_shst_enrty;
+    Symble_table symbol_table;
+    struct stat sb;
+    char *shst = NULL;
+    char file_name[128];
+    int fd;
+
+    fgets(file_name, 128, stdin);
+    sscanf(file_name, "%s\n", file_name);
+
+    fd = open(file_name, O_RDONLY);
+    if(fd == -1){
+        perror("Open file");
+        return;
+    }
+
+    if (fstat(fd, &sb) == -1){
+        perror("fstat");
+        close(fd);
+        return;
+    }
+
+    if(DEBUG_MODE != 0){
+        fprintf(stderr, "DEBUG: Set file name to %d\n", DEBUG_MODE);
+    }
+
+    file_map = mmap(0, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (file_map == MAP_FAILED){
+        close(fd);
+        perror("mmap");
+        return;
+    }
+    header = file_map;
+
+    section_header = file_map + header->e_shoff;
+    section_shst_enrty = section_header + header->e_shstrndx;
+    shst = file_map + section_shst_enrty->sh_offset;
+
+    fill_symble_table_struct(&symbol_table, file_map, section_header, header->e_shnum, shst);
+
+    print_symbol_table(&symbol_table, section_header, shst);
 
     munmap(file_map, sb.st_size);
     close(fd);
@@ -247,7 +352,7 @@ void display_menu(fun_desc function_desc[], int length){
 void menu(){
     int option = 0;
     fun_desc function_desc[] = {{"Toggle Debug Mode", toggle_debug_mode}, {"Examine ELF File", examine_elf_file},
-                                {"Print Section Names", print_section_names},
+                                {"Print Section Names", print_section_names}, {"Print Symbols", print_symbols},
                                 {"Quit", quit}, {NULL, NULL}};
     
     int menu_size = (int)(sizeof(function_desc)/sizeof(fun_desc) - 1);
